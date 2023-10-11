e8e4ae2e191e7681f2f2bc52c5733859e901813f
Snehan Kekre <snehan.minerva@gmail.com>
2023-10-11 13:32:56 +0530

    Add files via upload

---
+++ del.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.video(
+    "sample.mp4",
+    subtitles=[
+        {"lang": "en", "url": "sample_english.vtt"},
+        {"lang": "fr", "url": "sample_french.vtt"},
+        {"lang": "zh", "url": "sample_chinese.vtt"},
+        {"lang": "de", "url": "sample_german.vtt"},
+    ],
+)
